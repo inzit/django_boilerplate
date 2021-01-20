bb0aeefb397b52650325fc1de699ee68b0997269
Mike Abdala <mike@abdala.nl>
2021-01-20 12:34:45 +0100

    added import os to settings.example

--- boilerplate/settings.example.py
+++ boilerplate/settings.example.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 import ldap
 from django_auth_ldap.config import LDAPSearch, GroupOfNamesType, PosixGroupType, LDAPSearchUnion
 
